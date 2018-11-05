1962ea12c7b14cb5dface2f08352afffe1570f51
jonathantwite <13119611+jonathantwite@users.noreply.github.com>
2018-11-05 14:49:57 +0000

    Create ComponentViewModel-amd.js
    Initial commit

---
+++ ComponentViewModel-amd.js
@@ -0,0 +1,45 @@
+define(['knockout', 'text!./component-template.html'], function (ko, htmlTemplate) {
+    function ViewModel(params) {
+        var self = this;
+        
+        //#region Public Observables
+        self.isEditable = params.controlIsEditable;
+        
+        self.isVisible = ko.observable(false);
+        self.status = ko.pureComputed(function() {
+            return (self.isVisible() ? 'visible' : 'hidden') + ' and ' + (ko.unwrap(self.isEditable) ? 'editable' : 'readonly');
+        });
+        //#endregion
+        
+        //#region Private variables
+        var internalValue = 42;
+        //#endregion
+        
+        //#region Public functions
+        self.setVisible = function(visible) {
+            self.isVisible(visible);
+        };
+        //#endregion
+        
+        //#region Private functions
+        var strLength(str) {
+            return str.length;
+        };
+        //#endregion
+        
+        //#region Subscriptions
+        self.isVisibleSubscription = self.isVisible.subscribe(newValue) {
+            internalValue = newValue ? 12 : 42;
+        };
+        //#endregion
+        
+        //#region Disposal
+        self.dispose = function() {
+            self.isVisibleSubscription.dispose();
+        };
+        //#endregion
+        
+        //#region Initialisation
+        self.isVisible(internalValue === strLength('start'));
+        //#endregion
+    }
