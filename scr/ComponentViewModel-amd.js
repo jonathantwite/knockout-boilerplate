93c7d7448195e4527c174f57c953ca363e8d887a
jonathantwite <13119611+jonathantwite@users.noreply.github.com>
2018-11-06 15:50:32 +0000

    Create ComponentViewModel-amd.js

---
+++ scr/ComponentViewModel-amd.js
@@ -0,0 +1,37 @@
+define(['knockout', 'text!./component-template.html'], function (ko, htmlTemplate) {
+    function ViewModel(params) {
+        var self = this;
+        
+        //#region Public Observables
+        
+        //#endregion
+        
+        //#region Private variables
+        
+        //#endregion
+        
+        //#region Public functions
+        
+        //#endregion
+        
+        //#region Private functions
+        
+        //#endregion
+        
+        //#region Subscriptions
+        
+        //#endregion
+        
+        //#region Disposal
+        self.dispose = function() {
+        
+        };
+        //#endregion
+        
+        //#region Initialisation
+        
+        //#endregion
+    }
+    
+    return { viewModel: ViewModel, template: htmlTemplate };
+});
