83dab4d3678b44aa7c54cbdafb5759069dd218f6
jonathantwite <13119611+jonathantwite@users.noreply.github.com>
2018-11-06 15:46:02 +0000

    Create PlainViewModel-amd.js

---
+++ scr/PlainViewModel-amd.js
@@ -0,0 +1,35 @@
+define(['knockout'], function(ko) {
+    var ViewModel = function() {
+        var self = this;
+        
+        //#region Public Observables
+        
+        //#endregion
+    
+        //#region Private variables
+        
+        //#endregion
+        
+        //#region Public functions
+        
+        //#endregion
+        
+        //#region Private functions
+        
+        //#endregion
+        
+        //#region Subscriptions
+        
+        //#endregion
+        
+        //#region Disposal
+        self.dispose = function() {
+        
+        };
+        //#endregion
+        
+        //#region Initialisation
+        
+        //#endregion
+    }
+});
