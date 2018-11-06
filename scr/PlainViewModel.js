68293bf3ab9c18d666b1f3f9538698c80b0bc0c8
jonathantwite <13119611+jonathantwite@users.noreply.github.com>
2018-11-06 15:38:39 +0000

    Create PlainViewModel.js

---
+++ scr/PlainViewModel.js
@@ -0,0 +1,33 @@
+var ViewModel = function() {
+    var self = this;
+    
+    //#region Public Observables
+    
+    //#endregion
+    
+    //#region Private variables
+    
+    //#endregion
+    
+    //#region Public functions
+    
+    //#endregion
+    
+    //#region Private functions
+    
+    //#endregion
+    
+    //#region Subscriptions
+    
+    //#endregion
+    
+    //#region Disposal
+    self.dispose = function() {
+    
+    };
+    //#endregion
+    
+    //#region Initialisation
+    
+    //#endregion
+}
